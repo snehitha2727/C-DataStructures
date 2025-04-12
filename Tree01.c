2c362dd0c086674c33541fe5630e285a2acf1094
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-04-12 08:17:38 +0530

    Create Tree01.c

---
+++ Tree01.c
@@ -0,0 +1,32 @@
+//Binary Tree
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for a tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Function for Preorder Traversal
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+// Main function
+int main() {
+    struct Node* root = createNode(1);
+    root->
